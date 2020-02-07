abc1a7158550c9bb0c6d7d16f2c01d10e1ff3e93
janakhpon <jnovaxer@gmail.com>
2020-02-07 07:22:09 +0630

    GDEV006 components and design

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { TodoFormProps, TodoType } from 'types';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+export default function TodoSubmit({ todoList, handleTodoList }: TodoFormProps) {
+
+	const [todoInput, handleTodoInput] = useState<string>('');
+
+	const addTodo = (todoInput: string) => {
+		const todo: TodoType = createTodo(todoInput);
+		handleTodoList([...todoList, todo]);
+	}
+	const createTodo = (todoInput: string) => {
+		const lastTodoId = todoList.length > 0 ? todoList[todoList.length - 1].id : 1;
+		return { id: lastTodoId + 1, content: todoInput, completed: false };
+	}
+
+	return (
+		<div className="TodoSubmit">
+			<form onSubmit={(e) => e.preventDefault()}>
+				<TextField
+					className="SubmitInput"
+					label="Your task here ... "
+					type="search"
+					onChange={(e) => handleTodoInput(e.target.value)} />
+
+				<Button
+					className="SubmitButton"
+					variant="contained"
+					color="secondary"
+					onClick={() => addTodo(todoInput)}>
+					save
+				</Button>
+			</form>
+		</div>
+	)
+}
